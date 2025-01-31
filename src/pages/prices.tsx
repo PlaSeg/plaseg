7d49d5982d1fc84712ed83f4fda853f23421dbc2
Suzana <suzanasilva5430@gmail.com>
2025-01-31 16:21:38 -0300

    feat: página de preços criada. closes #3

--- src/pages/prices.tsx
+++ src/pages/prices.tsx
@@ -4,20 +4,20 @@ export function Prices() {
 	return (
 		<div className="w-full h-screen flex items-center justify-center">
 			<div className="bg-slate-100 w-[1200px] h-[500px] grid grid-cols-2 p-4
-			rounded-lg">
-				<div>
-					<strong>Bem-vindo(a) ao Plaseg</strong>
+			rounded-lg " >
+				<div className="space-y-2 m-4" >
+					<strong className="text-blue-500">Bem-vindo(a) ao Plaseg</strong>
 					<h1 className="text-5xl font-bold">Selecione um plano</h1>
 				</div>
 
-				<div className="space-y-4">
+				<div className="space-y-4 m-4">
 					<div
 						className="w-[300px] border-2 bg-white border-blue-500 p-2 rounded-md
 				flex justify-between"
 					>
 						<div className="flex flex-col">
-							<strong>Básico</strong>
-							<span>Esse é o plano báisco</span>
+							<strong>Trimestral</strong>
+							<span className="text-sm">Esse é o plano de 3 meses</span>
 						</div>
 
 						<div>
@@ -30,12 +30,12 @@ export function Prices() {
 				flex justify-between"
 					>
 						<div className="flex flex-col">
-							<strong>Básico</strong>
-							<span>Esse é o plano báisco</span>
+							<strong>Semestral</strong>
+							<span className="text-sm">Esse é o plano de 6 meses</span>
 						</div>
 
 						<div>
-							<strong>R$ 9.000</strong>
+							<strong>R$ 16.800</strong>
 						</div>
 					</div>
 
@@ -44,12 +44,12 @@ export function Prices() {
 				flex justify-between"
 					>
 						<div className="flex flex-col">
-							<strong>Básico</strong>
-							<span>Esse é o plano báisco</span>
+							<strong>Anual</strong>
+							<span className="text-sm">Esse é o plano de 12 meses</span>
 						</div>
 
 						<div>
-							<strong>R$ 9.000</strong>
+							<strong>R$ 30.000</strong>
 						</div>
 					</div>
 
