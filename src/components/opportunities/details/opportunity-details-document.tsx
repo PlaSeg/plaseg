7201451628f037af9b4f3403ca1cf8d3b0f9ac72
Izaías Lima <izaiaslima356@gmail.com>
2025-07-23 21:19:33 -0300

    fix: corrigir erros na build relacionados a documentos do projeto. fixes #185

--- src/components/opportunities/details/opportunity-details-document.tsx
+++ src/components/opportunities/details/opportunity-details-document.tsx
@@ -1,29 +1,29 @@
 import { Link } from "react-router";
 import { Button } from "@/components/ui/button";
-import type { RequiredDocument } from "@/hooks/opportunities/use-get-opportunity-by-id";
+import type { OpportunityById } from "@/hooks/opportunities/use-get-opportunity-by-id";
 
-interface OpportunityDetailsDocumentProps {
-	requiredDocument: RequiredDocument;
+interface OpportunityDetailsAttachmentProps {
+	attachment: OpportunityById["attachments"][number];
 }
 
-export function OpportunityDetailsDocument({
-	requiredDocument,
-}: OpportunityDetailsDocumentProps) {
+export function OpportunityDetailsAttachment({
+	attachment,
+}: OpportunityDetailsAttachmentProps) {
 	return (
 		<div
-			key={requiredDocument.id}
+			key={attachment.id}
 			className="flex justify-between items-center p-4 rounded-lg border border-slate-200
 			gap-6"
 		>
 			<div className="flex flex-col gap-2">
-				<span>{requiredDocument.name}</span>
+				<span>{attachment.name}</span>
 				<p className="text-muted-foreground text-sm">
-					{requiredDocument.description}
+					{attachment.description}
 				</p>
 			</div>
 
 			<Button variant="secondary" asChild>
-				<Link to={requiredDocument.model} target="_blank">
+				<Link to={attachment.model} target="_blank">
 					Ver modelo
 				</Link>
 			</Button>
